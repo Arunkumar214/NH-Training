e8ff79035a0e57637471f5de6111995816ef2b28
Arunkumar214 <arunkumaryadlapalli@gmail.com>
2022-01-25 15:53:22 +0530

    first submission

---
+++ Day 2 Project 2/Day 2 Project 2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2_Project_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=1;i<=10;i++)
+                Console.WriteLine(i);
+            Console.ReadLine();
+        }
+    }
+}
