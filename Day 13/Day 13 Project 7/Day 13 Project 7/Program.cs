88161f31fae4329cb3c08ee9b37e632feda8c874
Arunkumar214 <arunkumaryadlapalli@gmail.com>
2022-02-11 10:38:46 +0530

    Assignments

---
+++ Day 13/Day 13 Project 7/Day 13 Project 7/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_13_Project_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> data = new Stack<int> ();
+            data.Push(15);
+            data.Push(21);
+            data.Push(19);
+
+            Console.WriteLine(data.Count );
+            Console.WriteLine(data.Pop());
+            Console.WriteLine(data.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
