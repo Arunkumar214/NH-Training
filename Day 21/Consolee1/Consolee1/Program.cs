847145a2143700d67b32110e5b6ff3061753a274
Arunkumar214 <arunkumaryadlapalli@gmail.com>
2022-02-21 12:36:04 +0530

    21 Feb 2022

---
+++ Day 21/Consolee1/Consolee1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Consolee1.ServiceReference1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consolee1
+{
+    //Author: Arun  
+
+    //Purpose: Creating a web service and console application to consume that webservice  
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebService1SoapClient ws = new WebService1SoapClient ();
+
+            Console.WriteLine(ws.Factorial (5));
+            Console.WriteLine(ws.Add (2,2));
+            Console.WriteLine(ws.Mul(2, 2));
+            Console.WriteLine(ws.Div (2, 2));
+
+            Console.ReadLine();
+        }
+    }
+}
